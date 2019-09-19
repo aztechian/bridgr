f89d233005642c30c0e7856130d0b4dfac73d52c
Ian Martin <ian@imartin.net>
2019-09-19 09:47:40 -0600

    Docker Run() should call Setup() - Fixes #49 (#50)
    - additional change I had to quiet down Yum setup steps

--- internal/app/bridgr/assets/templates/yum.sh
+++ internal/app/bridgr/assets/templates/yum.sh
@@ -1,6 +1,6 @@
 set -e
-yum clean all
-yum install -y yum-plugin-downloadonly createrepo curl
+yum clean -y -q all
+yum install -y -q yum-plugin-downloadonly createrepo curl
 yumdownloader --resolve --archlist=x86_64 --destdir=/packages/7/x86_64 {{Join . " "}}
 cd /packages/7/x86_64
 echo "Creating YUM repository..."
